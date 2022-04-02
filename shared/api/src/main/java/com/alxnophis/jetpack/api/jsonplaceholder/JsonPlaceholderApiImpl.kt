d797ca3bac43a0587d5a54dd22a809d6c021c62d
alex <alxamejua@tutanota.com>
2022-04-02 15:44:54 +0200

    [feature/api_module] Add JsonPlaceholder API

--- shared/api/src/main/java/com/alxnophis/jetpack/api/jsonplaceholder/JsonPlaceholderApiImpl.kt
+++ shared/api/src/main/java/com/alxnophis/jetpack/api/jsonplaceholder/JsonPlaceholderApiImpl.kt
@@ -1,12 +1,38 @@
 package com.alxnophis.jetpack.api.jsonplaceholder
 
+import com.alxnophis.jetpack.api.jsonplaceholder.model.AlbumApiModel
+import com.alxnophis.jetpack.api.jsonplaceholder.model.CommentApiModel
 import com.alxnophis.jetpack.api.jsonplaceholder.model.ErrorResponse
+import com.alxnophis.jetpack.api.jsonplaceholder.model.PhotoApiModel
 import com.alxnophis.jetpack.api.jsonplaceholder.model.PostApiModel
+import com.alxnophis.jetpack.api.jsonplaceholder.model.UserApiModel
 import com.haroldadmin.cnradapter.NetworkResponse
 
 class JsonPlaceholderApiImpl(
     private val service: JsonPlaceholderRetrofitService
 ) : JsonPlaceholderApi {
 
-    override suspend fun getPost(): NetworkResponse<List<PostApiModel>, ErrorResponse> = service.getPosts()
+    override suspend fun getPost(): NetworkResponse<List<PostApiModel>, ErrorResponse> =
+        service.getPosts()
+
+    override suspend fun getPostById(id: Int): NetworkResponse<PostApiModel, ErrorResponse> =
+        service.getPostById(id)
+
+    override suspend fun getCommentsByPostId(postId: Int): NetworkResponse<List<CommentApiModel>, ErrorResponse> =
+        service.getCommentsByPostId(postId)
+
+    override suspend fun getUsers(): NetworkResponse<List<UserApiModel>, ErrorResponse> =
+        service.getUsers()
+
+    override suspend fun getUserById(id: Int): NetworkResponse<UserApiModel, ErrorResponse> =
+        service.getUsersBy(id)
+
+    override suspend fun getAlbums(): NetworkResponse<List<AlbumApiModel>, ErrorResponse> =
+        service.getAlbums()
+
+    override suspend fun getPhotos(): NetworkResponse<List<PhotoApiModel>, ErrorResponse> =
+        service.getPhotos()
+
+    override suspend fun getPhotosByAlbumId(albumId: Int): NetworkResponse<List<PhotoApiModel>, ErrorResponse> =
+        service.getPhotosByAlbumId(albumId)
 }
---
+++ shared/api/src/main/java/com/alxnophis/jetpack/api/jsonplaceholder/model/CommentApiModel.kt
@@ -0,0 +1,9 @@
+package com.alxnophis.jetpack.api.jsonplaceholder.model
+
+data class CommentApiModel(
+    val postId: Int,
+    val id: Int,
+    val name: String,
+    val email: String,
+    val body: String
+)
