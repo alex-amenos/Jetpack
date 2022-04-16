6c6715dc0ae35cf23d8cc3d37dc6022ecd84c985
alex <alxamejua@tutanota.com>
2022-04-16 17:12:09 +0200

    [feature/10-location-tracker] Unify TopBar on Core to be reused

--- feature/settings/src/main/java/com/alxnophis/jetpack/settings/ui/view/SettingsScreen.kt
+++ feature/settings/src/main/java/com/alxnophis/jetpack/settings/ui/view/SettingsScreen.kt
@@ -2,32 +2,20 @@ package com.alxnophis.jetpack.settings.ui.view
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.alxnophis.jetpack.core.extensions.getVersion
+import com.alxnophis.jetpack.core.ui.composable.CoreTopBar
 import com.alxnophis.jetpack.core.ui.theme.CoreTheme
 import com.alxnophis.jetpack.settings.R
 import com.alxnophis.jetpack.settings.ui.contract.SettingsEvent
@@ -62,7 +50,10 @@ internal fun Settings(
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
     ) {
-        SettingsTopBar(onSettingsEvent)
+        CoreTopBar(
+            title = stringResource(id = R.string.settings_title),
+            onBack = { onSettingsEvent.invoke(SettingsEvent.Finish) }
+        )
         Divider()
         SettingsNotificationItem(
             modifier = Modifier.fillMaxWidth(),
@@ -115,33 +106,6 @@ internal fun Settings(
             appVersion = appVersion
         )
         Divider()
-    }
-}
-
-@Composable
-internal fun SettingsTopBar(
-    onSettingsEvent: (event: SettingsEvent) -> Unit,
-) {
-    TopAppBar(
-        backgroundColor = MaterialTheme.colors.primaryVariant,
-        contentPadding = PaddingValues(start = 12.dp)
-    ) {
-        IconButton(
-            onClick = { onSettingsEvent.invoke(SettingsEvent.Finish) }
-        ) {
-            Icon(
-                imageVector = Icons.Default.ArrowBack,
-                contentDescription = stringResource(id = R.string.settings_cd_go_back),
-                tint = MaterialTheme.colors.onPrimary,
-            )
-        }
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(
-            text = stringResource(id = R.string.settings_title),
-            color = MaterialTheme.colors.onPrimary,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Medium,
-        )
     }
 }
 
---
+++ shared/core/src/main/java/com/alxnophis/jetpack/core/ui/composable/CoreTopBar.kt
@@ -0,0 +1,47 @@
+package com.alxnophis.jetpack.core.ui.composable
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.alxnophis.jetpack.core.R
+
+@Composable
+fun CoreTopBar(
+    title: String,
+    onBack: () -> Unit,
+) {
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.primaryVariant,
+        contentPadding = PaddingValues(start = 12.dp)
+    ) {
+        IconButton(
+            onClick = { onBack() }
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = stringResource(id = R.string.core_cd_go_back),
+                tint = MaterialTheme.colors.onPrimary,
+            )
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = title,
+            color = MaterialTheme.colors.onPrimary,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium,
+        )
+    }
+}
