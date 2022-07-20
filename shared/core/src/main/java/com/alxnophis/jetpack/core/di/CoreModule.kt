c644dae10e240917b50564042f313ff29b290fa8
alex <alxamejua@tutanota.com>
2022-07-20 21:11:09 +0200

    [feature/spacex] check ktlint

--- shared/core/src/main/java/com/alxnophis/jetpack/core/di/CoreModule.kt
+++ shared/core/src/main/java/com/alxnophis/jetpack/core/di/CoreModule.kt
@@ -4,7 +4,6 @@ import com.alxnophis.jetpack.core.base.formatter.DateFormatter
 import com.alxnophis.jetpack.kotlin.utils.DefaultDispatcherProvider
 import com.alxnophis.jetpack.kotlin.utils.DispatcherProvider
 import org.koin.core.module.Module
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val coreModule: Module = module() {
