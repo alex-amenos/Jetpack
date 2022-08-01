eb1a258d73a8e61eb8cdc748badf0c163f93c44f
alex <alxamejua@tutanota.com>
2022-08-01 18:08:58 +0200

    [feature/spacex_tests] add internal to SettingsTags

--- feature/settings/src/main/java/com/alxnophis/jetpack/settings/ui/view/SettingsTags.kt
+++ feature/settings/src/main/java/com/alxnophis/jetpack/settings/ui/view/SettingsTags.kt
@@ -1,6 +1,6 @@
 package com.alxnophis.jetpack.settings.ui.view
 
-object SettingsTags {
+internal object SettingsTags {
     const val TAG_TOGGLE_ITEM = "toggle_item"
     const val TAG_CHECK_ITEM = "check_item"
     const val TAG_MARKETING_OPTION = "marketing_option_"
