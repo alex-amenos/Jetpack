85002875b731125d39bc33c4ba95bb45127847fb
alex <alxamejua@tutanota.com>
2022-08-09 23:56:28 +0200

    [feture/maintenance] Update AuthenticationScreenTest

--- feature/authentication/src/androidTest/java/com/alxnophis/jetpack/authentication/ui/view/AuthenticationScreenTest.kt
+++ feature/authentication/src/androidTest/java/com/alxnophis/jetpack/authentication/ui/view/AuthenticationScreenTest.kt
@@ -3,7 +3,9 @@ package com.alxnophis.jetpack.authentication.ui.view
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.alxnophis.jetpack.authentication.R
+import com.alxnophis.jetpack.authentication.ui.contract.AuthenticationMode
 import com.alxnophis.jetpack.authentication.ui.contract.AuthenticationState
 import com.alxnophis.jetpack.testing.base.BaseComposeTest
 import org.junit.Test
@@ -11,19 +13,53 @@ import org.junit.Test
 @ExperimentalComposeUiApi
 class AuthenticationScreenTest : BaseComposeTest() {
 
-    private val screenState = AuthenticationState()
-
     @Test
     fun sign_in_title_displayed_by_default() {
+        authenticationContent(AuthenticationState())
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.authentication_label_sign_in_to_account))
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun need_account_displayed_and_clickable() {
+        authenticationContent(AuthenticationState())
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.authentication_action_need_account))
+            .assertIsDisplayed()
+            .performClick()
+    }
+
+    @Test
+    fun sign_in_title_displayed() {
+        authenticationContent(
+            AuthenticationState(authenticationMode = AuthenticationMode.SIGN_IN)
+        )
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.authentication_label_sign_in_to_account))
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun sign_up_title_displayed() {
+        authenticationContent(
+            AuthenticationState(authenticationMode = AuthenticationMode.SIGN_UP)
+        )
+
+        composeTestRule
+            .onNodeWithText(context.getString(R.string.authentication_label_sign_up_for_account))
+            .assertIsDisplayed()
+    }
+
+    private fun authenticationContent(state: AuthenticationState) {
         composeTestRule.setContent {
             AuthenticationContent(
-                authenticationState = screenState,
+                authenticationState = state,
                 onAuthenticationEvent = {}
             )
         }
-
-        composeTestRule
-            .onNodeWithText(targetContext.getString(R.string.authentication_label_sign_in_to_account))
-            .assertIsDisplayed()
     }
 }
