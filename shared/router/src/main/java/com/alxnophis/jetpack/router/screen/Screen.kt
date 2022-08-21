228cb9b3836b82c7e39312baa88680e91bced3c6
alex <alxamejua@tutanota.com>
2022-08-21 19:26:13 +0200

    [feature/nav_arguments] organize screen code

--- shared/router/src/main/java/com/alxnophis/jetpack/router/screen/Screen.kt
+++ shared/router/src/main/java/com/alxnophis/jetpack/router/screen/Screen.kt
@@ -3,13 +3,15 @@ package com.alxnophis.jetpack.router.screen
 // Routes
 const val ROOT_ROUTE = "root"
 const val AUTHENTICATION_ROUTE = "authentication"
-const val AUTHENTICATION_ARGUMENT_EMAIL = "email"
 const val GAME_BALL_CLICKER_ROUTE = "game-ball-clicker"
 const val HOME_ROUTE = "home"
 const val LOCATION_TRACKER_ROUTE = "location-tracker"
 const val POSTS_ROUTE = "posts"
 const val SETTINGS_ROUTE = "settings"
 const val SPACEX_ROUTE = "spacex"
+
+// Arguments
+const val AUTHENTICATION_ARGUMENT_EMAIL = "email"
 
 // Screens
 sealed class Screen(val route: String) {
