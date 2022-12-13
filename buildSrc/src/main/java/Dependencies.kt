f7e49d3bb4d1de896a2f353cf91976e5f2ad73a1
Alex <alxnophis@pm.me>
2022-12-13 21:49:54 +0100

    [feature/improvements] update compose bom to 2022.11.00

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,12 +1,13 @@
-@file:Suppress( "MemberVisibilityCanBePrivate", "SpellCheckingInspection")
- object Version {
+@file:Suppress("MemberVisibilityCanBePrivate", "SpellCheckingInspection")
+
+object Version {
     const val ANDROIDX_ANNOTATIONS = "1.4.0"
     const val ANDROIDX_ACTIVITY = "1.5.0"
     const val ANDROIDX_APPCOMPAT = "1.5.1"
     const val ANDROIDX_ARCH_CORE_COMMON = "2.1.0"
     const val ANDROIDX_ARCH_CORE_RUNTIME = "2.1.0"
     const val ANDROIDX_COMPOSE = "1.2.1"
-    const val ANDROIDX_COMPOSE_BOM = "2022.10.00"
+    const val ANDROIDX_COMPOSE_BOM = "2022.11.00"
     const val ANDROIDX_COMPOSE_COMPILER = "1.3.2"
     const val ANDROIDX_COMPOSE_CONSTRAINT_LAYOUT = "1.0.1"
     const val ANDROIDX_COMPOSE_NAVIGATION = "2.5.2"
