5f2ac703244a05352332a8dd1999aff36652e650
Alex <alxnophis@pm.me>
2022-12-19 20:10:09 +0100

    [feature/fix_concepts]  remove unneeded datasource on post module

--- feature/posts/src/main/java/com/alxnophis/jetpack/posts/data/datasource/PostDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.alxnophis.jetpack.posts.data.datasource
-
-import arrow.core.Either
-import com.alxnophis.jetpack.api.jsonplaceholder.model.PostApiModel
-import com.alxnophis.jetpack.posts.domain.model.PostsError
-
-interface PostDataSource {
-    suspend fun getPosts(): Either<PostsError, List<PostApiModel>>
-}
