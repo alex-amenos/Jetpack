6695d71d60d3f7f98e8eb0979c47aec72da6dbb3
Alex <alxnophis@pm.me>
2022-12-19 20:26:30 +0100

    [feature/fix_concepts] remove unneeded datasource on spacex module

--- feature/spacex/src/main/java/com/alxnophis/jetpack/spacex/data/datasource/LaunchesDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.alxnophis.jetpack.spacex.data.datasource
-
-import arrow.core.Either
-import com.alxnophis.jetpack.spacex.LaunchesQuery
-import com.alxnophis.jetpack.spacex.data.model.LaunchesError
-
-internal interface LaunchesDataSource {
-    suspend fun getPastLaunches(hasToFetchDataFromNetworkOnly: Boolean): Either<LaunchesError, LaunchesQuery.Data?>
-}
