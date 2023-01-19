8a87acc3c36c367d0b858db304b006bb020bba4f
Alex <alxnophis@pm.me>
2023-01-19 23:39:29 +0100

    [feature/improve_jetpack_navigation] Update MyPlaygroundScreen

--- feature/my-playground/src/main/java/com/alxnophis/jetpack/myplayground/ui/view/MyPlaygroundScreen.kt
+++ feature/my-playground/src/main/java/com/alxnophis/jetpack/myplayground/ui/view/MyPlaygroundScreen.kt
@@ -1,14 +1,18 @@
 package com.alxnophis.jetpack.myplayground.ui.view
 
+import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,6 +22,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.alxnophis.jetpack.core.ui.composable.CoreTopBar
 import com.alxnophis.jetpack.core.ui.theme.AppTheme
+import com.alxnophis.jetpack.core.ui.theme.mediumPadding
 import com.alxnophis.jetpack.myplayground.R
 
 @Composable
@@ -27,46 +32,54 @@ fun MyPlaygroundScreen(
     BackHandler {
         popBackStack()
     }
-    MyPlaygroundContent(navigateBack = popBackStack)
+    MyPlaygroundScaffold(navigateBack = popBackStack)
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-internal fun MyPlaygroundContent(
+internal fun MyPlaygroundScaffold(
     navigateBack: () -> Unit
 ) {
     AppTheme {
-        AppTheme {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colors.surface),
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            scaffoldState = rememberScaffoldState(),
+            topBar = {
                 CoreTopBar(
                     modifier = Modifier.fillMaxWidth(),
                     title = stringResource(id = R.string.myplayground_title),
                     onBack = { navigateBack() },
                 )
-                Text(
-                    modifier = Modifier
-                        .wrapContentHeight()
-                        .weight(0.1f),
-                    style = MaterialTheme.typography.h3,
-                    fontWeight = FontWeight.Bold,
-                    color = MaterialTheme.colors.secondary,
-                    textAlign = TextAlign.Center,
-                    text = "Let's PLAY!",
-                )
-            }
-        }
+            },
+        ) { _ -> MyPlaygroundContent() }
+    }
+}
+
+@Composable
+internal fun MyPlaygroundContent() {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colors.surface)
+            .fillMaxSize()
+            .padding(mediumPadding),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.wrapContentHeight(),
+            style = MaterialTheme.typography.h3,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.secondary,
+            textAlign = TextAlign.Center,
+            text = "Let's PLAY!",
+        )
     }
 }
 
 @Preview
 @Composable
-private fun MyPlaygroundScreenPreview() {
-    MyPlaygroundContent(
+private fun MyPlaygroundScaffoldPreview() {
+    MyPlaygroundScaffold(
         navigateBack = {}
     )
 }
