8714877b558fdc3938bb02c11c35a90e2b0a4f1c
Alex <alxnophis@pm.me>
2023-02-24 23:54:32 +0100

    [feature/56-download-a-file-using-downloadmanager] style[posts]: specify returning type on usecase

--- feature/posts/src/main/java/com/alxnophis/jetpack/posts/domain/usecase/PostsUseCase.kt
+++ feature/posts/src/main/java/com/alxnophis/jetpack/posts/domain/usecase/PostsUseCase.kt
@@ -1,9 +1,12 @@
 package com.alxnophis.jetpack.posts.domain.usecase
 
+import arrow.core.Either
 import com.alxnophis.jetpack.posts.data.repository.PostsRepository
+import com.alxnophis.jetpack.posts.domain.model.Post
+import com.alxnophis.jetpack.posts.domain.model.PostsError
 
 class PostsUseCase(
     private val repository: PostsRepository
 ) {
-    suspend operator fun invoke() = repository.getPosts()
+    suspend operator fun invoke(): Either<PostsError, List<Post>> = repository.getPosts()
 }
