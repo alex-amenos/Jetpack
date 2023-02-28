3d44a35b4717699255f9caa841c8b9062a5f2e69
Alex <alxnophis@pm.me>
2023-02-28 22:24:07 +0100

    [fix/mother] chore[root]: remove DI on root module

--- feature/root/src/main/java/com/alxnophis/jetpack/root/ui/RootActivity.kt
+++ feature/root/src/main/java/com/alxnophis/jetpack/root/ui/RootActivity.kt
@@ -3,18 +3,12 @@ package com.alxnophis.jetpack.root.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.alxnophis.jetpack.root.di.injectRoot
 import com.alxnophis.jetpack.root.ui.navigation.SetupNavGraph
-import com.alxnophis.jetpack.root.ui.viewmodel.RootViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RootActivity : ComponentActivity() {
 
-    private val viewModel: RootViewModel by viewModel()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        injectRoot()
         renderContent()
     }
 
