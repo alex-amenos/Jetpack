0558f1eeabc5f0bf6243e0f2030438c11cad20de
Alex <alxnophis@pm.me>
2023-03-01 22:51:20 +0100

    [feature/arrow-retrofit] feature: replace post API using EitherCallAdapterFactory by Arrow on retrofit

--- shared/api/src/main/java/com/alxnophis/jetpack/api/jsonplaceholder/model/JsonPlaceholderApiError.kt
+++
@@ -1,8 +0,0 @@
-package com.alxnophis.jetpack.api.jsonplaceholder.model
-
-sealed class JsonPlaceholderApiError {
-    object Network : JsonPlaceholderApiError()
-    object Unexpected : JsonPlaceholderApiError()
-    data class Server(val statusCode: Int?) : JsonPlaceholderApiError()
-    data class Unknown(val statusCode: Int?) : JsonPlaceholderApiError()
-}
