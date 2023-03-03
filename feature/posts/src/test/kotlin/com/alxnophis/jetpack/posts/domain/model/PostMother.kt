5dd8bafb54bb7138c538d8ceaf6118553d9f27a5
Alex <alxnophis@pm.me>
2023-03-03 15:37:05 +0100

    [feature/arrow-retrofit] test[posts]: Add repository unit tests

---
+++ feature/posts/src/test/kotlin/com/alxnophis/jetpack/posts/domain/model/PostMother.kt
@@ -0,0 +1,19 @@
+package com.alxnophis.jetpack.posts.domain.model
+
+import com.alxnophis.jetpack.kotlin.constants.EMPTY
+import com.alxnophis.jetpack.kotlin.constants.ZERO_INT
+
+internal object PostMother {
+
+    operator fun invoke(
+        id: Int = ZERO_INT,
+        userId: Int = ZERO_INT,
+        title: String = EMPTY,
+        body: String = EMPTY
+    ) = Post(
+        id = id,
+        userId = userId,
+        title = title,
+        body = body
+    )
+}
