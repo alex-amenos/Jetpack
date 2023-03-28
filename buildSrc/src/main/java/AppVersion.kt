464f6bd1f9cb4046add91ffb8d226b003cde2f32
Alex <alxnophis@pm.me>
2023-03-28 23:24:31 +0200

    [develop] chore: Update app version to 0.2.0

--- buildSrc/src/main/java/AppVersion.kt
+++ buildSrc/src/main/java/AppVersion.kt
@@ -2,7 +2,7 @@
 
 object AppVersion {
     private const val MAJOR = 0
-    private const val MINOR = 1
+    private const val MINOR = 2
     private const val BUILD = 0
 
     const val NAME: String = "$MAJOR.$MINOR.$BUILD"
