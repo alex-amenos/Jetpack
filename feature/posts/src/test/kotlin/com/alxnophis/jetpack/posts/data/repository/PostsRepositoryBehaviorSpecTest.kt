7217f75475ee64205bdb0765962cd69fef500136
Alex <alxnophis@pm.me>
2023-04-05 00:42:51 +0200

    [feature/kotest] tests[posts]: adds PostsRepository kotests
    https://kotlintesting.com/kotest-parameterized/

--- feature/posts/src/test/kotlin/com/alxnophis/jetpack/posts/data/repository/PostsRepositoryBehaviorSpecTest.kt
+++ feature/posts/src/test/kotlin/com/alxnophis/jetpack/posts/data/repository/PostsRepositoryBehaviorSpecTest.kt
@@ -1,57 +1,60 @@
 package com.alxnophis.jetpack.posts.data.repository
 
+import arrow.core.left
 import arrow.core.right
 import com.alxnophis.jetpack.api.jsonplaceholder.JsonPlaceholderRetrofitService
+import com.alxnophis.jetpack.api.jsonplaceholder.model.CallErrorMother
 import com.alxnophis.jetpack.api.jsonplaceholder.model.PostApiModelMother
 import com.alxnophis.jetpack.posts.domain.model.PostMother
+import com.alxnophis.jetpack.posts.domain.model.PostsError
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestCoroutineScheduler
-import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.runTest
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
-private class PostsRepositoryBehaviorSpecTest : BehaviorSpec(
-    {
-        val POST_API_1 = PostApiModelMother(id = 1, userId = 1, title = "title1", body = "body1")
-        val POST_API_2 = PostApiModelMother(id = 2, userId = 2, title = "title2", body = "body2")
-        val POST_API_LIST = listOf(POST_API_1, POST_API_2)
-        val POST_1 = PostMother(id = 1, userId = 1, title = "title1", body = "body1")
-        val POST_2 = PostMother(id = 2, userId = 2, title = "title2", body = "body2")
-        val POST_LIST = listOf(POST_1, POST_2)
+internal class PostsRepositoryBehaviorSpecTest : BehaviorSpec({
 
-        val testScheduler = TestCoroutineScheduler()
-        val testDispatcher = StandardTestDispatcher(testScheduler)
-        val testScope = TestScope(testDispatcher)
+    val postApi1 = PostApiModelMother(id = 1, userId = 1, title = "title1", body = "body1")
+    val postApi2 = PostApiModelMother(id = 2, userId = 2, title = "title2", body = "body2")
+    val postApiList = listOf(postApi1, postApi2)
+    val post1 = PostMother(id = 1, userId = 1, title = "title1", body = "body1")
+    val post2 = PostMother(id = 2, userId = 2, title = "title2", body = "body2")
+    val postList = listOf(post1, post2)
 
-        lateinit var apiDataSourceMock: JsonPlaceholderRetrofitService
-        lateinit var repositoryMock: PostsRepository
-        beforeContainer {
-            apiDataSourceMock = mock()
+    coroutineTestScope = true
+
+    Given("a posts repository") {
+        val apiDataSourceMock: JsonPlaceholderRetrofitService = mock()
+        val repository: PostsRepository = PostsRepositoryImpl(
+            apiDataSource = apiDataSourceMock
+        )
+
+        When("Get posts from API succeed") {
+            whenever(apiDataSourceMock.getPosts()).thenReturn(postApiList.right())
+            val result = repository.getPosts()
+
+            Then("the result should be a list of posts") {
+                result shouldBe postList.right()
+            }
         }
-        Given("PostsRepository") {
-            testScope.runTest {
-                repositoryMock = PostsRepositoryImpl(
-                    ioDispatcher = testDispatcher,
-                    apiDataSource = apiDataSourceMock
-                )
-            }
-            When("true") {
-                Then("true") {
-                    true shouldBe true
-                }
-            }
-            When("get posts from API succeed") {
-                whenever(apiDataSourceMock.getPosts()).thenReturn(POST_API_LIST.right())
-                val result = repositoryMock.getPosts()
-                Then("returns a list of posts") {
-                    result shouldBe POST_LIST.right()
+
+        listOf(
+            CallErrorMother.ioError() to PostsError.Unexpected,
+            CallErrorMother.unexpectedCallError() to PostsError.Unexpected,
+            CallErrorMother.httpError(code = 300) to PostsError.Network,
+            CallErrorMother.httpError(code = 400) to PostsError.Network,
+            CallErrorMother.httpError(code = 500) to PostsError.Server
+        ).forEach { (apiError, expectedError) ->
+            When("get post from API fails with $apiError") {
+                whenever(apiDataSourceMock.getPosts()).thenReturn(apiError.left())
+                val result = repository.getPosts()
+
+                Then("the result should be $expectedError") {
+                    result shouldBe expectedError.left()
                 }
             }
         }
     }
-)
+})
