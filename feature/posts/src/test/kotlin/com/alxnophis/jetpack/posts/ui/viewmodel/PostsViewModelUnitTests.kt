a54905cfc30baaf1be0b515b118a6b338404e266
Alex <alxnophis@pm.me>
2023-04-11 23:37:23 +0200

    [feature/kotest] test[post]: update PostsViewModel unit tests

--- feature/posts/src/test/kotlin/com/alxnophis/jetpack/posts/ui/viewmodel/PostsViewModelUnitTests.kt
+++ feature/posts/src/test/kotlin/com/alxnophis/jetpack/posts/ui/viewmodel/PostsViewModelUnitTests.kt
@@ -11,7 +11,7 @@ import com.alxnophis.jetpack.posts.domain.usecase.PostsUseCase
 import com.alxnophis.jetpack.posts.ui.contract.PostsEvent
 import com.alxnophis.jetpack.posts.ui.contract.PostsState
 import com.alxnophis.jetpack.testing.listener.MainCoroutineListener
-import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -19,7 +19,7 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 @ExperimentalCoroutinesApi
-class PostsViewModelUnitTests : BehaviorSpec({
+class PostsViewModelUnitTests : FunSpec({
 
     val post1 = PostMother(id = 1, userId = 1, title = "title1", body = "body1")
     val post2 = PostMother(id = 2, userId = 2, title = "title2", body = "body2")
@@ -29,72 +29,74 @@ class PostsViewModelUnitTests : BehaviorSpec({
 
     coroutineTestScope = true
 
-    Given("a PostsViewModel") {
+    test(
+        "GIVEN a PostsViewModel with default initial state " +
+            "WHEN get post succeed " +
+            "THEN show AND hide loading AND post state should end with a list of posts"
+    ) {
+        val postUseCaseMock: PostsUseCase = mock()
+        val viewModel = PostsViewModel(postsUseCase = postUseCaseMock)
+
+        whenever(postUseCaseMock.invoke()).thenReturn(postList.right())
+
+        viewModel.uiState.test {
+            awaitItem() shouldBe PostsState()
+            awaitItem() shouldBe PostsState(isLoading = true)
+            awaitItem() shouldBe PostsState(isLoading = false, posts = postList)
+            expectNoEvents()
+        }
+    }
+
+    listOf(
+        PostsError.Network to R.string.posts_error_network,
+        PostsError.Server to R.string.posts_error_server,
+        PostsError.Unknown to R.string.posts_error_unknown,
+        PostsError.Unexpected to R.string.posts_error_unexpected
+    ).forEach { (error, errorMessage) ->
+        test(
+            "GIVEN a PostsViewModel with default initial state " +
+                "WHEN get post fails with ${error::class.simpleName} error" +
+                "THEN show AND hide loading AND post state result should be an error with messageId $errorMessage"
+        ) {
+            val errorId = 1L
+            val postUseCaseMock: PostsUseCase = mock()
+            val viewModel = PostsViewModel(
+                postsUseCase = postUseCaseMock,
+                getRandomUUID = { errorId }
+            )
+
+            whenever(postUseCaseMock.invoke()).thenReturn(error.left())
+
+            viewModel.uiState.test {
+                awaitItem() shouldBe PostsState()
+                awaitItem() shouldBe PostsState(isLoading = true)
+                awaitItem() shouldBe PostsState(
+                    isLoading = false,
+                    errorMessages = listOf(ErrorMessage(id = errorId, messageId = errorMessage))
+                )
+                expectNoEvents()
+            }
+        }
+    }
+
+    test(
+        "GIVEN a PostViewModel initialized with an error " +
+            "WHEN DismissError event " +
+            "THEN post state result should be without errors"
+    ) {
         val postUseCaseMock: PostsUseCase = mock()
         val errorId = 1L
+        val viewModel = PostsViewModel(
+            getRandomUUID = { errorId },
+            postsUseCase = postUseCaseMock
+        )
+        whenever(postUseCaseMock.invoke()).thenReturn(PostsError.Network.left())
 
-        When("init view model succeed") {
-            val viewModel = PostsViewModel(postsUseCase = postUseCaseMock)
-            whenever(postUseCaseMock.invoke()).thenReturn(postList.right())
+        viewModel.handleEvent(PostsEvent.DismissError(errorId))
 
-            Then("result should be a list of posts") {
-                viewModel.uiState.test {
-                    awaitItem() shouldBe PostsState()
-                    awaitItem() shouldBe PostsState(isLoading = true)
-                    awaitItem() shouldBe PostsState(isLoading = false, posts = postList)
-                    expectNoEvents()
-                }
-            }
-        }
-
-        listOf(
-            PostsError.Network to R.string.posts_error_network,
-            PostsError.Server to R.string.posts_error_server,
-            PostsError.Unknown to R.string.posts_error_unknown,
-            PostsError.Unexpected to R.string.posts_error_unexpected
-        ).forEach { (error, errorMessage) ->
-            When("init view model fails getting post with $error") {
-                val viewModel = PostsViewModel(
-                    initialState = PostsState(),
-                    getRandomUUID = { errorId },
-                    postsUseCase = postUseCaseMock
-                )
-                whenever(postUseCaseMock.invoke()).thenReturn(error.left())
-
-                Then("result should be an error with messageId $errorMessage") {
-                    viewModel.uiState.test {
-                        awaitItem() shouldBe PostsState()
-                        awaitItem() shouldBe PostsState(isLoading = true)
-                        awaitItem() shouldBe PostsState(
-                            isLoading = false,
-                            errorMessages = listOf(ErrorMessage(id = errorId, messageId = errorMessage))
-                        )
-                        expectNoEvents()
-                    }
-                }
-            }
-        }
-
-        xWhen("dismiss error from view model") {
-            whenever(postUseCaseMock.invoke()).thenReturn(PostsError.Network.left())
-            val viewModel = PostsViewModel(
-                initialState = PostsState(),
-                getRandomUUID = { errorId },
-                postsUseCase = postUseCaseMock
-            )
-            viewModel.handleEvent(PostsEvent.DismissError(errorId))
-
-            Then("result should be a state without errors") {
-                viewModel.uiState.test {
-                    awaitItem() shouldBe PostsState()
-                    awaitItem() shouldBe PostsState(isLoading = true)
-                    awaitItem() shouldBe PostsState(
-                        errorMessages = listOf(ErrorMessage(id = errorId, messageId = R.string.posts_error_network))
-                    )
-                    awaitItem() shouldBe PostsState()
-                    expectNoEvents()
-                }
-            }
+        viewModel.uiState.test {
+            expectMostRecentItem() shouldBe PostsState()
+            expectNoEvents()
         }
     }
 })
