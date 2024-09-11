ebf0e5c7fb6d2ebf1d132b9ee5a093f506367a7f
Alex <alxnophis@pm.me>
2024-09-11 23:01:29 +0200

    [feature/push_notifications] chore
    move notifications navigation graph to ui folder

--- feature/root/src/main/java/com/alxnophis/jetpack/root/ui/navigation/NavGraph.kt
+++ feature/root/src/main/java/com/alxnophis/jetpack/root/ui/navigation/NavGraph.kt
@@ -14,7 +14,7 @@ import com.alxnophis.jetpack.game.ballclicker.ui.navigation.ballClickerNavGraph
 import com.alxnophis.jetpack.home.ui.navigation.homeNavGraph
 import com.alxnophis.jetpack.location.tracker.ui.navigation.locationTrackerNavGraph
 import com.alxnophis.jetpack.myplayground.navigation.myPlaygroundNavGraph
-import com.alxnophis.jetpack.notifications.navigation.notificationsNavGraph
+import com.alxnophis.jetpack.notifications.ui.navigation.notificationsNavGraph
 import com.alxnophis.jetpack.posts.ui.navigation.postsNavGraph
 import com.alxnophis.jetpack.router.screen.Route
 import com.alxnophis.jetpack.settings.ui.navigation.settingsNavGraph
