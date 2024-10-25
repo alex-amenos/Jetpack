7b5f39a4330123a63a8c8da6b198308ef032e942
Alex <alxnophis@pm.me>
2024-10-25 22:10:06 +0200

    [feature/screenshoot_testing] refactor[posts]
    updates screenshot testing

--- feature/posts/src/screenshotTest/java/com/alxnophis/jetpack/posts/ui/view/PostPreviewsScreenshots.kt
+++ feature/posts/src/screenshotTest/java/com/alxnophis/jetpack/posts/ui/view/PostPreviewsScreenshots.kt
@@ -6,12 +6,10 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.alxnophis.jetpack.posts.ui.contract.PostsUiState
 import com.alxnophis.jetpack.posts.ui.view.provider.PostStateProvider
 
-internal class PostPreviewsScreenshots {
-    @Preview(showBackground = true)
-    @Composable
-    private fun PostScreenTesting(
-        @PreviewParameter(PostStateProvider::class) state: PostsUiState,
-    ) {
-        PostsScreen(state)
-    }
+@Preview(showBackground = true)
+@Composable
+private fun PostScreenTesting(
+    @PreviewParameter(PostStateProvider::class) state: PostsUiState,
+) {
+    PostsScreen(state)
 }
