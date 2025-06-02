601f51db1b03725b923d5748b0248e135e94aa6f
Alex <alxnophis@pm.me>
2025-06-03 00:05:41 +0200

    [feature/settings_datastore] feat[core]
    creates a crypto object

---
+++ shared/core/src/main/java/com/alxnophis/jetpack/core/base/crypto/Crypto.kt
@@ -0,0 +1,62 @@
+package com.alxnophis.jetpack.core.base.crypto
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import com.alxnophis.jetpack.kotlin.constants.ZERO_INT
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
+
+object Crypto {
+    private const val KEY_ALIAS = "secret"
+    private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
+    private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
+    private const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
+    private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
+
+    private val cipher = Cipher.getInstance(TRANSFORMATION)
+    private val keyStore =
+        KeyStore
+            .getInstance("AndroidKeyStore")
+            .apply { load(null) }
+
+    private fun getKey(): SecretKey =
+        if (keyStore.containsAlias(KEY_ALIAS)) {
+            keyStore.getKey(KEY_ALIAS, null) as SecretKey
+        } else {
+            createKey()
+        }
+
+    private fun createKey(): SecretKey =
+        KeyGenerator
+            .getInstance(ALGORITHM)
+            .apply {
+                init(
+                    KeyGenParameterSpec
+                        .Builder(
+                            KEY_ALIAS,
+                            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT,
+                        ).setBlockModes(BLOCK_MODE)
+                        .setEncryptionPaddings(PADDING)
+                        .setRandomizedEncryptionRequired(true)
+                        .setUserAuthenticationRequired(false)
+                        .build(),
+                )
+            }.generateKey()
+
+    fun encrypt(bytes: ByteArray): ByteArray {
+        cipher.init(Cipher.ENCRYPT_MODE, getKey())
+        val iv = cipher.iv
+        val encryptedData = cipher.doFinal(bytes)
+        return iv + encryptedData
+    }
+
+    fun decrypt(bytes: ByteArray): ByteArray {
+        val iv = bytes.copyOfRange(ZERO_INT, cipher.blockSize)
+        val data = bytes.copyOfRange(cipher.blockSize, bytes.size)
+        cipher.init(Cipher.DECRYPT_MODE, getKey(), IvParameterSpec(iv))
+        return cipher.doFinal(data)
+    }
+}
