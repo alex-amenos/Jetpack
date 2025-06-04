32d74611dd0f1174a8810ddadf870b950977c390
Alex <alxnophis@pm.me>
2025-06-04 21:57:33 +0200

    [feature/settings_datastore] fix[core]
    fix cipher issues

--- shared/core/src/main/java/com/alxnophis/jetpack/core/base/crypto/Crypto.kt
+++ shared/core/src/main/java/com/alxnophis/jetpack/core/base/crypto/Crypto.kt
@@ -2,6 +2,7 @@ package com.alxnophis.jetpack.core.base.crypto
 
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import com.alxnophis.jetpack.core.BuildConfig
 import com.alxnophis.jetpack.kotlin.constants.ZERO_INT
 import java.security.KeyStore
 import javax.crypto.Cipher
@@ -10,13 +11,12 @@ import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
 
 object Crypto {
-    private const val KEY_ALIAS = "secret"
+    private const val KEY_ALIAS = BuildConfig.APP_KEY_ALIAS
     private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
     private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
     private const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
     private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
 
-    private val cipher = Cipher.getInstance(TRANSFORMATION)
     private val keyStore =
         KeyStore
             .getInstance("AndroidKeyStore")
@@ -47,6 +47,7 @@ object Crypto {
             }.generateKey()
 
     fun encrypt(bytes: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance(TRANSFORMATION)
         cipher.init(Cipher.ENCRYPT_MODE, getKey())
         val iv = cipher.iv
         val encryptedData = cipher.doFinal(bytes)
@@ -54,6 +55,7 @@ object Crypto {
     }
 
     fun decrypt(bytes: ByteArray): ByteArray {
+        val cipher = Cipher.getInstance(TRANSFORMATION)
         val iv = bytes.copyOfRange(ZERO_INT, cipher.blockSize)
         val data = bytes.copyOfRange(cipher.blockSize, bytes.size)
         cipher.init(Cipher.DECRYPT_MODE, getKey(), IvParameterSpec(iv))
