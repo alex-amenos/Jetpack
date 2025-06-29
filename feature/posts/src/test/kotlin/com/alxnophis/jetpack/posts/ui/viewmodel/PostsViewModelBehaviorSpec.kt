533fdcaba1119fe801e361a2cde93043adc775bf
Alex <alxnophis@pm.me>
2025-06-29 23:51:18 +0200

    [feature/navigation3] [feature/navigation3] test: fix unit tests

--- feature/posts/src/test/kotlin/com/alxnophis/jetpack/posts/ui/viewmodel/PostsViewModelBehaviorSpec.kt
+++ feature/posts/src/test/kotlin/com/alxnophis/jetpack/posts/ui/viewmodel/PostsViewModelBehaviorSpec.kt
@@ -12,12 +12,15 @@ import com.alxnophis.jetpack.posts.ui.contract.PostsStatus
 import com.alxnophis.jetpack.posts.ui.contract.PostsUiState
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.reset
 import org.mockito.kotlin.whenever
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class PostsViewModelBehaviorSpec : BehaviorSpec() {
     private val postsRepositoryMock: PostsRepository = mock()
 
@@ -66,6 +69,8 @@ internal class PostsViewModelBehaviorSpec : BehaviorSpec() {
 
                     Then("uiState should reflect error with appropriate error type") {
                         runTest {
+                            runCurrent()
+
                             viewModel.uiState.test {
                                 awaitItem() shouldBe
                                     PostsUiState.initialState.copy(
