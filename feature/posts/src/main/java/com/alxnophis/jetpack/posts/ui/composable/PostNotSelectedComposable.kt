3550e801eaf9a0390b9c9744efc802a9104f0e3d
Alex <alxnophis@pm.me>
2025-06-29 22:40:47 +0200

    [feature/navigation3] refactor: update color usage in PostNotSelectedComposable

--- feature/posts/src/main/java/com/alxnophis/jetpack/posts/ui/composable/PostNotSelectedComposable.kt
+++ feature/posts/src/main/java/com/alxnophis/jetpack/posts/ui/composable/PostNotSelectedComposable.kt
@@ -1,5 +1,6 @@
 package com.alxnophis.jetpack.posts.ui.composable
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,7 +53,7 @@ fun PostNotSelectedComposable(modifier: Modifier = Modifier) {
                 Text(
                     text = stringResource(R.string.posts_post_not_selected_detail),
                     style = MaterialTheme.typography.bodyMedium,
-                    color = Color.Gray,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
                 )
             }
         }
