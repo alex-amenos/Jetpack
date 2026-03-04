ffdb428f639d0137fc41db24d54fd03c13313cfc
Alex <alxnophis@pm.me>
2026-03-04 23:41:11 +0100

    [copilot/migrate-to-agp-9-0] refactor: update Gradle configurations and dependencies for AGP 9.0 migration

--- feature/posts/src/main/java/com/alxnophis/jetpack/posts/data/model/Post.kt
+++ feature/posts/src/main/java/com/alxnophis/jetpack/posts/data/model/Post.kt
@@ -1,15 +1,11 @@
 package com.alxnophis.jetpack.posts.data.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Post(
     val id: Int,
     val userId: Int,
     val title: String,
     val body: String,
-) : Parcelable {
+) {
     val titleCapitalized: String
         get() = title.replaceFirstChar { it.uppercase() }
 }
