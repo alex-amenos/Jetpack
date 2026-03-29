741187890529bb596214fe9374a7295ee0df7d44
Alex <alxnophis@pm.me>
2026-03-29 23:20:36 +0200

    [refactor/post_fixes] refactor: implement PostsDataSource and PostsRemoteDataSource for improved data handling

---
+++ feature/posts/src/main/java/com/alxnophis/jetpack/posts/data/datasource/PostsDataSource.kt
@@ -0,0 +1,12 @@
+package com.alxnophis.jetpack.posts.data.datasource
+
+import arrow.core.Either
+import com.alxnophis.jetpack.posts.data.model.Post
+import com.alxnophis.jetpack.posts.data.model.PostDetailError
+import com.alxnophis.jetpack.posts.data.model.PostsError
+
+interface PostsDataSource {
+    suspend fun getPosts(): Either<PostsError, List<Post>>
+
+    suspend fun getPostById(postId: Int): Either<PostDetailError, Post>
+}
