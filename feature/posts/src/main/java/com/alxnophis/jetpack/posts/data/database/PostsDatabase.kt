134a731f40efcff1122e39ac37545cb6c3a06fc8
Alex <alxnophis@pm.me>
2026-04-13 22:41:57 +0200

    [refactor/postsNetworkBoundResource] refactor: rename database version from 2 to 1 for PostsDatabase

--- feature/posts/src/main/java/com/alxnophis/jetpack/posts/data/database/PostsDatabase.kt
+++ feature/posts/src/main/java/com/alxnophis/jetpack/posts/data/database/PostsDatabase.kt
@@ -7,7 +7,7 @@ import com.alxnophis.jetpack.posts.data.database.entity.PostsMetadataEntity
 
 @Database(
     entities = [PostEntity::class, PostsMetadataEntity::class],
-    version = 2,
+    version = 1,
     exportSchema = true,
 )
 internal abstract class PostsDatabase : RoomDatabase() {
