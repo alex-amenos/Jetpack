d4efbef8ea0c9507ce72adbd3209fd9e11bab521
Alex <alxnophis@pm.me>
2026-04-29 23:21:37 +0200

    [fix/file-downloader] fix: add window insets padding to Snackbar and adjust padding for better layout

--- feature/file-downloader/src/main/java/com/alxnophis/jetpack/filedownloader/ui/composable/FileDownloaderScreen.kt
+++ feature/file-downloader/src/main/java/com/alxnophis/jetpack/filedownloader/ui/composable/FileDownloaderScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -307,10 +308,12 @@ private fun SnackbarError(
     Box(modifier) {
         SnackbarHost(
             hostState = snackbarHostState,
-            modifier = Modifier.align(Alignment.BottomCenter),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .windowInsetsPadding(WindowInsets.safeDrawing),
         ) { snackbarData: SnackbarData ->
             Snackbar(
-                modifier = Modifier.padding(10.dp),
+                modifier = Modifier.padding(mediumPadding),
                 action = {},
             ) {
                 Text(snackbarData.visuals.message)
