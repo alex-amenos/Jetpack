f4ba9c009fb4286fb609428301167e270d58f4d4
Alex <alxnophis@pm.me>
2026-06-04 21:42:11 +0200

    [feat/map_location] feat: simplify coroutine context usage in LocationRepositoryImpl

--- feature/location-tracker/src/main/java/com/alxnophis/jetpack/location/tracker/data/repository/LocationRepositoryImpl.kt
+++ feature/location-tracker/src/main/java/com/alxnophis/jetpack/location/tracker/data/repository/LocationRepositoryImpl.kt
@@ -4,32 +4,19 @@ import arrow.core.Either
 import com.alxnophis.jetpack.location.tracker.data.data.LocationDataSource
 import com.alxnophis.jetpack.location.tracker.data.model.Location
 import com.alxnophis.jetpack.location.tracker.data.model.LocationParameters
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.withContext
 
 internal class LocationRepositoryImpl(
     private val locationDataSource: LocationDataSource,
-    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : LocationRepository {
-    override val locationSharedFlow: SharedFlow<Location> =
-        locationDataSource.locationSharedFlow
+    override val locationSharedFlow: SharedFlow<Location> = locationDataSource.locationSharedFlow
 
     override fun provideLastKnownLocationFlow(): Flow<Location?> = locationDataSource.provideLastKnownLocationFlow()
 
     override fun hasLocationAvailable(): Either<Unit, Unit> = locationDataSource.hasLocationAvailable()
 
-    override suspend fun startLocationProvider(locationParameters: LocationParameters) {
-        withContext(ioDispatcher) {
-            locationDataSource.startLocationProvider(locationParameters)
-        }
-    }
+    override suspend fun startLocationProvider(locationParameters: LocationParameters) = locationDataSource.startLocationProvider(locationParameters)
 
-    override suspend fun stopLocationProvider() {
-        withContext(ioDispatcher) {
-            locationDataSource.stopLocationProvider()
-        }
-    }
+    override suspend fun stopLocationProvider() = locationDataSource.stopLocationProvider()
 }
