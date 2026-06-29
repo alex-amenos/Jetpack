0d5e67a8813adaf629ef07b8c3852726f38c7203
Alex <alxnophis@pm.me>
2026-06-29 23:14:47 +0200

    [copilot/create-ui-tests-movies-module] fix(tests): update import for createAndroidComposeRule to v2 in PostsJourneyRobotTest

--- app/src/androidTest/java/com/alxnophis/jetpack/e2e/PostsJourneyRobotTest.kt
+++ app/src/androidTest/java/com/alxnophis/jetpack/e2e/PostsJourneyRobotTest.kt
@@ -1,6 +1,6 @@
 package com.alxnophis.jetpack.e2e
 
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.alxnophis.jetpack.robot.homeScreen
 import com.alxnophis.jetpack.robot.postDetailScreen
