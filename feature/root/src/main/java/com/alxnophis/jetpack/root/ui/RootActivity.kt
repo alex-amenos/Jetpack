c578a8c1425bdad5014dbeace38339a99089692d
Alex <alxnophis@pm.me>
2026-09-01 23:25:17 +0200

    [feature/maestro] feat(movies): add UI flow and assertions for Movies screen interactions

--- feature/root/src/main/java/com/alxnophis/jetpack/root/ui/RootActivity.kt
+++ feature/root/src/main/java/com/alxnophis/jetpack/root/ui/RootActivity.kt
@@ -7,11 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import com.alxnophis.jetpack.core.ui.theme.AppTheme
 import com.alxnophis.jetpack.root.ui.navigation.Navigation
 
 class RootActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
@@ -20,6 +24,7 @@ class RootActivity : ComponentActivity() {
                 Navigation(
                     modifier =
                         Modifier
+                            .semantics { testTagsAsResourceId = true }
                             .fillMaxSize()
                             .background(MaterialTheme.colorScheme.surface),
                 )
