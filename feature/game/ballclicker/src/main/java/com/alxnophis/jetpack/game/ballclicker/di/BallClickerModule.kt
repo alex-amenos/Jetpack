a2803ab35427af0df3a1926fae05a0c170e140c7
Alex <alxnophis@pm.me>
2026-09-13 12:48:01 +0200

    [feature/structured-coroutines] refactor(ball-clicker): improve coroutine handling and streamline game logic in BallClickerViewModel

--- feature/game/ballclicker/src/main/java/com/alxnophis/jetpack/game/ballclicker/di/BallClickerModule.kt
+++ feature/game/ballclicker/src/main/java/com/alxnophis/jetpack/game/ballclicker/di/BallClickerModule.kt
@@ -3,12 +3,11 @@ package com.alxnophis.jetpack.game.ballclicker.di
 import com.alxnophis.jetpack.game.ballclicker.domain.usecase.BallClickerTimerUseCase
 import com.alxnophis.jetpack.game.ballclicker.ui.viewmodel.BallClickerViewModel
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val ballClickerModule: Module =
     module {
-        factoryOf(::BallClickerTimerUseCase)
+        factory { BallClickerTimerUseCase() }
         viewModel { BallClickerViewModel(get()) }
     }
